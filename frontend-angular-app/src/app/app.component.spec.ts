d936971f231dc90a6bd438b09f8342b0d899d1e9
JayPi89 <56943962+JayPi89@users.noreply.github.com>
2024-01-20 13:31:47 +0100

    added node modules

--- frontend-angular-app/src/app/app.component.spec.ts
+++ frontend-angular-app/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'quizapp'`, () => {
+  it(`should have as title 'frontendangularapp'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('quizapp');
+    expect(app.title).toEqual('frontendangularapp');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('quizapp app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('frontendangularapp app is running!');
   });
 });
